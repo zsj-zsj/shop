1b17e4a60b0f414e76d85328df8f76dd85e572da
zhangshaojie <2211045409@qq.com>
2020-04-08 08:42:43 +0000

    debug

--- resources/views/admin/user/login.blade.php
+++ resources/views/admin/user/login.blade.php
@@ -9,7 +9,7 @@
 	<form action="{{url('admin/login_do')}}" method="post">
 		@csrf
 		账号:<input type="text" name="account" placeholder="用户名/邮箱/电话"><br>
-	    密码:<input type="password" name="pass"><a href="">忘记密码</a><br>
+	    密码:<input type="password" name="pass"><a href="{{url('pass')}}">忘记密码</a><br>
 	    <input type="submit" value="登录">
 	</form>
 
